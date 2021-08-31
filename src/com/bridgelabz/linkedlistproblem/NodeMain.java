b82dd9e4cc2b6b5130e5d84c3987346450a2cf34
Vinay288 <vinayhiremath288@gmail.com>
2021-08-31 18:22:01 +0530

    ADDED:function call to search and delete function

--- src/com/bridgelabz/linkedlistproblem/NodeMain.java
+++ src/com/bridgelabz/linkedlistproblem/NodeMain.java
@@ -7,11 +7,14 @@ public class NodeMain {
 		Node<Integer> firstNode = new Node(56);
 		Node<Integer> secondNode = new Node(30);
 		Node<Integer> thirdNode = new Node(70);
-		MyLinkedList myLinkedList = new MyLinkedList();
+		Node<Integer> fourthNode= new Node(40);
+		MyLinkedList<Integer> myLinkedList = new MyLinkedList();
 		myLinkedList.append(firstNode);
 		myLinkedList.append(secondNode);
 		myLinkedList.append(thirdNode);
-		myLinkedList.find(30);
+		myLinkedList.append(fourthNode);
+		myLinkedList.delete(40);
+		myLinkedList.printMyNodes();
 	}
 
 }
