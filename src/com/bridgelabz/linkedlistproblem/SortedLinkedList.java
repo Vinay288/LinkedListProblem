b6d90781b2966f1024648f293873f0fc1d85b0a2
Vinay288 <vinayhiremath288@gmail.com>
2021-08-31 18:52:29 +0530

    ADDED:SortedLinkedList class which implements comparable and method to append which sorts internally

---
+++ src/com/bridgelabz/linkedlistproblem/SortedLinkedList.java
@@ -0,0 +1,55 @@
+package com.bridgelabz.linkedlistproblem;
+
+public class SortedLinkedList<K extends Comparable<K>> {
+private K key;
+private INode head,tail;
+public SortedLinkedList() {
+	this.tail=null;
+	this.head=null;
+}
+public void sort() {
+	INode<K> current = head, index = null;
+	  
+    K temp;
+
+    if (head == null) {
+        return;
+    }
+    else {
+        while (current != null) {
+            index = current.getNext();
+
+            while (index != null) {
+                if (current.getKey().compareTo(index.getKey())>0) {
+                    temp = current.getKey();
+                    current.setKey(index.getKey());
+                    index.setKey(temp);
+                }
+
+                index = index.getNext();
+            }
+            current = current.getNext();
+        }
+    }
+}
+public void append(INode<K> newNode) {
+	sort();
+	if (tail == null)
+		this.tail = newNode;
+	if (head == null)
+		this.head = newNode;
+	else
+		tail.setNext(newNode);
+	tail = newNode;
+}
+public void printMyNodes() {
+	INode tempNode = head;
+	while (tempNode != null) {
+		System.out.print(tempNode.getKey() + "->");
+		tempNode = tempNode.getNext();
+	}
+	System.out.print("null");
+}
+
+
+}
