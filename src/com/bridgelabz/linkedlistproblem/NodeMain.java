25a3a151768229a01e7c008f661aebcbe4d06440
Vinay288 <vinayhiremath288@gmail.com>
2021-08-31 14:09:43 +0530

    ADDED:created simple linked list

--- src/com/bridgelabz/linkedlistproblem/NodeMain.java
+++ src/com/bridgelabz/linkedlistproblem/NodeMain.java
@@ -3,11 +3,13 @@ package com.bridgelabz.linkedlistproblem;
 public class NodeMain {
 
 	public static void main(String[] args) {
+		
 		Node<Integer> firstNode = new Node(56);
 		Node<Integer> secondNode = new Node(30);
 		Node<Integer> thirdNode = new Node(70);
 		firstNode.next = secondNode;
 		secondNode.next = thirdNode;
+		
 		System.out.println(firstNode.key+"->"+firstNode.next.key+"->"+firstNode.next.next.key);
 
 	}
