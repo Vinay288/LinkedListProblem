b6fe773f207460f827f6d638c1bedc546290a107
Vinay288 <vinayhiremath288@gmail.com>
2021-08-31 13:53:43 +0530

    ADDED:project initialization

---
+++ src/com/bridgelabz/linkedlistproblem/Node.java
@@ -0,0 +1,5 @@
+package com.bridgelabz.linkedlistproblem;
+
+public class Node {
+
+}
---
+++ src/com/bridgelabz/linkedlistproblem/NodeMain.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.linkedlistproblem;
+
+public class NodeMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
